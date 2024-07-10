ea46b812661ae1e524ff93a5d74f916a54cca094
Mathieu Declercq <mat.declercq@gmail.com>
2024-07-10 10:25:24 +0200

    Create postgres_model.js

---
+++ postgres_model.js
@@ -0,0 +1,6 @@
+var pg_port=1212;
+var pg_host="gitguardians.com:9082/BLUDB";
+var pg_user="root";
+var pg_pass="su&1235èffdgrstr0ngpassG";
+
+var mongo_uri = "mongodb+srv://otheruser:kjn§'hjI!&12@gg-is-awesome-gg273.mongodb.net/test?retryWrites=true&w=majority";

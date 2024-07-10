ef1ce0b32c757a584f2b4c9185137151cc83889f
Mathieu Declercq <mat.declercq@gmail.com>
2024-07-10 13:00:41 +0200

    Update postgres_model.js

--- postgres_model.js
+++ postgres_model.js
@@ -6,3 +6,5 @@ var pg_pass="su&1235èffdgrstr0ngpassG";
 var mongo_uri = "mongodb+srv://otheruser:kjn§'hjI!&12@gg-is-awesome-gg273.mongodb.net/test?retryWrites=true&w=majority";
 
 var mongo_replica_uri = "mongodb+srv://newuser:12ssde&@2@gg-is-awesome-gg273.mongodb.net/test?retryWrites=true&w=majority";
+
+var mongo_preprod_uri = "mongodb+srv://preprod:12s!ède&s2@gg-is-awesome-gg273.mongodb.net/test?retryWrites=true&w=majority";
